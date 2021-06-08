5f8c3d67e1f5b043ff120cc517b5de23496858a9
matrix98 <srki.rado@gmail.com>
2021-06-08 21:16:23 +0200

    fixed timestamps

--- src/Traits/TimestampableEntity.php
+++ src/Traits/TimestampableEntity.php
@@ -9,25 +9,15 @@ trait TimestampableEntity
 {
     /**
      * @var ?\DateTime
-     * @ODM\Field(type="date", nullable=true)
+     * @ODM\Field(type="date")
      */
-    private ?\DateTime $createdAt;
+    private ?\DateTime $createdAt = null;
 
     /**
      * @var \DateTime
      * @ODM\Field(type="date")
      */
     private \DateTime $updatedAt;
-
-    public function __call($method, $args) : void
-    {
-        if (str_contains(strtolower($method), 'set')){
-            if ($this->createdAt === null){
-                $this->createdAt = new \DateTime();
-            }
-            $this->updatedAt = new \DateTime();
-        }
-    }
 
     /**
      * @return \DateTime
@@ -43,5 +33,29 @@ trait TimestampableEntity
     public function getUpdatedAt(): \DateTime
     {
         return $this->updatedAt;
+    }
+
+    /**
+     * @param \DateTime $createdAt
+     */
+    public function setCreatedAt(\DateTime $createdAt) : void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @param \DateTime $updatedAt
+     */
+    public function setUpdatedAt(\DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function setTimestamps() : void
+    {
+        if ($this->createdAt !== null){
+            $this->createdAt = new \DateTime();
+        }
+        $this->updatedAt = new \DateTime();
     }
 }
